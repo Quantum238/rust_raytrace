f8a1094d22bfb4a269e259c186bff4aaeeaecd15
Danny <pinghedm@gmail.com>
2019-01-07 22:37:23 -0500

    lemme seem those normals

--- src/main.rs
+++ src/main.rs
@@ -217,24 +217,28 @@ impl Ray {
 		self.A + t * self.B
 	}
 }
-
+M8G6ZiAmSbUP
 fn color(r: Ray) -> Vec3{
-	if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &r){
-		return Vec3::new(1.0, 0.0, 0.0)
+	let t = hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &r);
+	if t > 0.0{
+		let N = Vec3::unit_vector(r.point_at_parameter(t) - Vec3::new(0.0, 0.0, -1.0));
+		return 0.5 * Vec3::new(N.x + 1.0, N.y + 1.0, N.z + 1.0);
 	}
 	let unit_direction = Vec3::unit_vector_from_ref(r.direction());
 	let t = 0.5 * (unit_direction.y  + 1.0);
 	(1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
-fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool{
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> f64{
 	let oc = *r.origin() - center;
 	let a = Vec3::dot(*r.direction(), *r.direction());
 	let b = 2.0 * Vec3::dot(oc, *r.direction());
 	let c = Vec3::dot(oc, oc) - radius * radius;
 	let discriminant = b * b - 4.0 * a * c;
-	discriminant > 0.0
+	if discriminant < 0.0{
+		return -1.0;
+	}
+	(-b - discriminant.sqrt()) / (2.0 * a)
 }
-
 fn main() {
 	let mut file = File::create("out/image.ppm").expect("Hey why can't I write?!");
 	let mut output = String::new();
