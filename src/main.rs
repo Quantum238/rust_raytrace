5eff5a7e5f3419ad1684002d687452df6cd6e6db
Danny <pinghedm@gmail.com>
2019-01-07 22:30:16 -0500

    HA, red sphere

--- src/main.rs
+++ src/main.rs
@@ -124,10 +124,20 @@ impl Add for Vec3 {
 		}
 	}
 }
-impl Sub for Vec3 {
+impl Sub<Vec3> for Vec3 {
 	type Output = Vec3;
 
 	fn sub(self, other: Vec3) -> Vec3 {
+		Vec3 {
+			x: self.x - other.x,
+			y: self.y - other.y,
+			z: self.z - other.z,
+		}
+	}
+}
+impl Sub<&Vec3> for Vec3 {
+	type Output = Vec3;
+	fn sub(self, other: &Vec3) -> Vec3{
 		Vec3 {
 			x: self.x - other.x,
 			y: self.y - other.y,
@@ -209,12 +219,20 @@ impl Ray {
 }
 
 fn color(r: Ray) -> Vec3{
+	if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, &r){
+		return Vec3::new(1.0, 0.0, 0.0)
+	}
 	let unit_direction = Vec3::unit_vector_from_ref(r.direction());
 	let t = 0.5 * (unit_direction.y  + 1.0);
 	(1.0 - t) * Vec3::new(1.0, 1.0, 1.0) + t * Vec3::new(0.5, 0.7, 1.0)
 }
-fn hit_sphere(center: &Vec3, radius: f64, r: &Ray){
-	
+fn hit_sphere(center: &Vec3, radius: f64, r: &Ray) -> bool{
+	let oc = *r.origin() - center;
+	let a = Vec3::dot(*r.direction(), *r.direction());
+	let b = 2.0 * Vec3::dot(oc, *r.direction());
+	let c = Vec3::dot(oc, oc) - radius * radius;
+	let discriminant = b * b - 4.0 * a * c;
+	discriminant > 0.0
 }
 
 fn main() {
